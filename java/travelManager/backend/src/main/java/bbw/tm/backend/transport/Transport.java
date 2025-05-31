8b1519c3086daf783f6a921b68cec04548474461
JinraGit <lars.hostettler@hotmail.de>
2025-05-31 10:20:16 +0200

    Change `id` field type from `Long` to `Integer` across multiple entities and add `TripRepository` with custom query method.

--- java/travelManager/backend/src/main/java/bbw/tm/backend/transport/Transport.java
+++ java/travelManager/backend/src/main/java/bbw/tm/backend/transport/Transport.java
@@ -14,7 +14,7 @@ public class Transport {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Integer id;
 
     @Column(nullable = false)
     private String type;
