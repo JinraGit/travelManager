4f7f6f0b13736a1239f46a21592dea0e9dd95960
JinraGit <lars.hostettler@hotmail.de>
2025-06-06 12:48:02 +0200

    Introduce `Address` entity, `AddressDTO`, and `AddressRepository` to manage address-related data and enhance modularity.

---
+++ java/travelManager/backend/src/main/java/bbw/tm/backend/address/AddressDTO.java
@@ -0,0 +1,12 @@
+package bbw.tm.backend.address;
+
+import lombok.Data;
+
+@Data
+public class AddressDTO {
+    private String street;
+    private String number;
+    private String city;
+    private String zipcode;
+}
+
---
+++ java/travelManager/backend/src/main/java/bbw/tm/backend/address/AddressRepository.java
@@ -0,0 +1,5 @@
+package bbw.tm.backend.address;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AddressRepository extends JpaRepository<Address, Integer> {}
