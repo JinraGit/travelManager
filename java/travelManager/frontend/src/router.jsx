f04d934358dd43164d8c0d71b1f9859e43d94a90
Rasim <zarzir@bbcag.ch>
2025-06-20 13:53:24 +0200

    Create und Overview in router.jsx eingebunden

--- java/travelManager/frontend/src/router.jsx
+++ java/travelManager/frontend/src/router.jsx
@@ -1,32 +1,39 @@
-import {createBrowserRouter} from 'react-router-dom';
+import { createBrowserRouter } from 'react-router-dom';
 import MainLayout from "@/layouts/MainLayout.jsx";
-import {ThemeProvider} from "@/context/ThemeContext.jsx";
+import { ThemeProvider } from "@/context/ThemeContext.jsx";
 import NotFoundRoute from "./routes/NotFoundRoute.jsx";
 import ProtectedRoute from "./routes/auth/ProtectedRoute";
 import SignInRoute from './routes/auth/SignInRoute';
 import SignUpRoute from './routes/auth/SignUpRoute';
 import AuthLayout from "@/layouts/AuthLayout.jsx";
 import CreateAdminRoute from "@/routes/admin/CreateAdminRoute.jsx";
-import WelcomeRoute from "@/routes/dashboard/WelcomeRoute.jsx";
 import ManageAccountsRoute from "@/routes/admin/ManageAccountsRoute.jsx";
-
+import TripOverviewRoute from "@/routes/dashboard/TripOverviewRoute.jsx";
+import CreateTripRoute from "@/routes/dashboard/CreateTripRoute.jsx";
 
 const router = createBrowserRouter([
     {
         element: (
             <ProtectedRoute>
                 <ThemeProvider>
-                    <MainLayout/>
+                    <MainLayout />
                 </ThemeProvider>
             </ProtectedRoute>
         ),
         children: [
-            {path: "/", element: <WelcomeRoute/>},
+            {
+                path: "trips/create",
+                element: <CreateTripRoute/>
+            },
+            {
+                path: "/trips/all",
+                element: <TripOverviewRoute />
+            },
             {
                 path: "/admin/create-admin",
                 element: (
                     <ProtectedRoute allowedRoles={["ADMIN"]}>
-                        <CreateAdminRoute/>
+                        <CreateAdminRoute />
                     </ProtectedRoute>
                 ),
             },
@@ -34,13 +41,13 @@ const router = createBrowserRouter([
                 path: "/admin/manage-accounts",
                 element: (
                     <ProtectedRoute allowedRoles={["ADMIN"]}>
-                        <ManageAccountsRoute/>
+                        <ManageAccountsRoute />
                     </ProtectedRoute>
                 ),
             },
             {
                 path: "*",
-                element: <NotFoundRoute/>,
+                element: <NotFoundRoute />,
             },
         ],
     },
@@ -48,7 +55,7 @@ const router = createBrowserRouter([
         path: "/accounts/signin",
         element: (
             <AuthLayout>
-                <SignInRoute/>
+                <SignInRoute />
             </AuthLayout>
         ),
         action: SignInRoute.action,
@@ -57,12 +64,11 @@ const router = createBrowserRouter([
         path: "/accounts/signup",
         element: (
             <AuthLayout>
-                <SignUpRoute/>
+                <SignUpRoute />
             </AuthLayout>
         ),
         action: SignUpRoute.action,
     },
-
 ]);
 
 export default router;
