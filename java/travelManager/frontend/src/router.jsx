9cb7a37afbc8f0a75f10fb7face899646f257757
JinraGit <lars.hostettler@hotmail.de>
2025-06-26 18:45:21 +0200

    Remove unused assets and correct formatting in `router.jsx`
    - Delete unused assets: `react.svg`, `vite.svg`, and `DefaultProfilePicture.png`.
    - Fix minor formatting issue in `router.jsx` for consistency.

--- java/travelManager/frontend/src/router.jsx
+++ java/travelManager/frontend/src/router.jsx
@@ -23,7 +23,7 @@ const router = createBrowserRouter([
         element: (
             <ProtectedRoute>
                 <ThemeProvider>
-                    <MainLayout />
+                   <MainLayout />
                 </ThemeProvider>
             </ProtectedRoute>
         ),
